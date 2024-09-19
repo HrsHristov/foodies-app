6462d6d40907824b3f989302d010b37b7aecb032
Hristo Hristov <hrshristov@gmail.com>
2024-09-19 17:41:03 +0300

    loading refactor to use Suspsense componenet

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,11 +1,17 @@
+import { Suspense } from "react";
 import Link from "next/link";
-import classes from "./page.module.css";
 import MealsGrid from "@/components/meals/meals-grid";
 import { getMeals } from "@/lib/meals";
 
-const MealsPage = async () => {
+import classes from "./page.module.css";
+
+const Meals = async () => {
     const meals = await getMeals();
 
+    return <MealsGrid meals={meals} />;
+};
+
+const MealsPage = () => {
     return (
         <>
             <header className={classes.header}>
@@ -22,7 +28,13 @@ const MealsPage = async () => {
                 </p>
             </header>
             <main className={classes.main}>
-                <MealsGrid meals={meals} />
+                <Suspense
+                    fallback={
+                        <p className={classes.loading}>Fetching meals...</p>
+                    }
+                >
+                    <Meals />
+                </Suspense>
             </main>
         </>
     );
