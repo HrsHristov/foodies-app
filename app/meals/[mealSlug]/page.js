aae092c1ac1f318203c87251c2fdbd2585491cd0
Hristo Hristov <hrshristov@gmail.com>
2024-09-20 14:39:13 +0300

    Fetch meal

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,22 +1,37 @@
 import Image from "next/image";
 import classes from "./page.module.css";
+import { getMeal } from "@/lib/meals";
 
-const MealDetailsPage = () => {
+const MealDetailsPage = ({ params }) => {
+    const meal = getMeal(params.mealSlug);
+
+    meal.instructions = meal.instructions.replace(/\n/g, "<br />");
+
     return (
         <>
-            <header className={classes.header}></header>
-            <div className={classes.image}>
-                <Image fill />
-            </div>
-            <div className={classes.headerText}>
-                <h1>TITLE</h1>
-                <p className={classes.creator}>
-                    by <a href={`mailto:${"EMAIL"}`}>NAME</a>
-                </p>
-                <p className={classes.summary}>Summary</p>
-            </div>
+            <header className={classes.header}>
+                <div className={classes.image}>
+                    <Image src={meal.image} alt={meal.title} fill />
+                </div>
+
+                <div className={classes.headerText}>
+                    <h1>{meal.title}</h1>
+                    <p className={classes.creator}>
+                        by{" "}
+                        <a href={`mailto:${meal.creator_email}`}>
+                            {meal.creator}
+                        </a>
+                    </p>
+                    <p className={classes.summary}>{meal.summary}</p>
+                </div>
+            </header>
             <main>
-                <p className={classes.instructions}></p>
+                <p
+                    className={classes.instructions}
+                    dangerouslySetInnerHTML={{
+                        __html: meal.instructions,
+                    }}
+                ></p>
             </main>
         </>
     );
