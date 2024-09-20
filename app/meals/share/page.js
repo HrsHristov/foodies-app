7c08d4bcc9cc268c5947d70a2fff5c727c4d4498
Hristo Hristov <hrshristov@gmail.com>
2024-09-20 16:11:47 +0300

    Write meal to the DB

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,4 +1,6 @@
 import ImagePicker from "@/components/meals/image-picker";
+import { shareMeal } from "@/lib/actions";
+
 import classes from "./page.module.css";
 
 const ShareMeal = () => {
@@ -12,7 +14,7 @@ const ShareMeal = () => {
                 <p>Or any other meal you feel needs sharing!</p>
             </header>
             <main className={classes.main}>
-                <form className={classes.form}>
+                <form className={classes.form} action={shareMeal}>
                     <div className={classes.row}>
                         <p>
                             <label htmlFor="name">Your name</label>
@@ -50,7 +52,7 @@ const ShareMeal = () => {
                             required
                         ></textarea>
                     </p>
-                    <ImagePicker />
+                    <ImagePicker label="Your image" name="image" />
                     <p className={classes.actions}>
                         <button type="submit">Share Meal</button>
                     </p>
