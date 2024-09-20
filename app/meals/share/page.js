5a1ac00affee61500dfa8a5619513d1a76c95ca9
Hristo Hristov <hrshristov@gmail.com>
2024-09-20 16:34:21 +0300

    Add useActionState Hook

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,9 +1,15 @@
+"use client";
+
+import { useActionState } from "react";
+
 import ImagePicker from "@/components/meals/image-picker";
 import { shareMeal } from "@/lib/actions";
 
+import MealsFormSubmit from "@/components/meals/meals-form-submit";
 import classes from "./page.module.css";
 
 const ShareMeal = () => {
+    const [state, formAction] = useActionState(shareMeal, { message: null });
     return (
         <>
             <header className={classes.header}>
@@ -14,7 +20,7 @@ const ShareMeal = () => {
                 <p>Or any other meal you feel needs sharing!</p>
             </header>
             <main className={classes.main}>
-                <form className={classes.form} action={shareMeal}>
+                <form className={classes.form} action={formAction}>
                     <div className={classes.row}>
                         <p>
                             <label htmlFor="name">Your name</label>
@@ -53,8 +59,9 @@ const ShareMeal = () => {
                         ></textarea>
                     </p>
                     <ImagePicker label="Your image" name="image" />
+                    {state.message && <p>{state.message}</p>}
                     <p className={classes.actions}>
-                        <button type="submit">Share Meal</button>
+                        <MealsFormSubmit />
                     </p>
                 </form>
             </main>
