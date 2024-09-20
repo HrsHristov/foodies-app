92d7a4390c7566ee91d460d93aaa4022e75cca26
Hristo Hristov <hrshristov@gmail.com>
2024-09-20 14:23:48 +0300

    Start working on Meal page

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,5 +1,25 @@
+import Image from "next/image";
+import classes from "./page.module.css";
+
 const MealDetailsPage = () => {
-    return <h1>Meal Details</h1>;
+    return (
+        <>
+            <header className={classes.header}></header>
+            <div className={classes.image}>
+                <Image fill />
+            </div>
+            <div className={classes.headerText}>
+                <h1>TITLE</h1>
+                <p className={classes.creator}>
+                    by <a href={`mailto:${"EMAIL"}`}>NAME</a>
+                </p>
+                <p className={classes.summary}>Summary</p>
+            </div>
+            <main>
+                <p className={classes.instructions}></p>
+            </main>
+        </>
+    );
 };
 
 export default MealDetailsPage;
